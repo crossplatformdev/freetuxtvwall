09779c61b1b644a23aa7d620e8d42fb32b12ef5c
Elías A. Angulo Klein <elias.angulo.klein@gmail.com>
2025-02-08 09:34:28 +0100

    favicon

--- app/functions/index.js
+++ app/functions/index.js
@@ -30,7 +30,7 @@ let links = [
     '<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">',
     '<script src="//code.jquery.com/jquery-1.10.2.js"></script>',
     '<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>',
-    '<link rel="icon" type="image/jpg" href="/public/images/favicon.jpg"/>'
+    '<link rel="icon" type="image/png" href="/public/images/favicon.png"/>'
 
     ];
 
